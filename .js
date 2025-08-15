74cbb35a3b3697ecc4ef415e957074105ed09f8b
eduardo_3k_ <kubiakoski.rocha@escola.pr.gov.br>
2025-08-14 21:08:10 -0300

    g

---
+++ .js
@@ -0,0 +1,15 @@
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)<script src="app.js"></script>
+<script src="perguntas.js"></script>function criaCartao(categoria, pergunta, resposta) {
+    console.log(categoria, pergunta, resposta)
+}
